1b9c215bd7af02333e5679490923b4f15d093dcd
Jamie Lentin <jm@lentin.co.uk>
2013-02-25 18:37:05 +0000

    WIP: Views that generate most of what a JS implementation needs

--- tutorweb/quiz/browser/lecture.py
+++ tutorweb/quiz/browser/lecture.py
@@ -1,5 +1,8 @@
+import json
+
 from AccessControl import getSecurityManager
 
+from zope.app.component.hooks import getSite
 from zope.component import getUtility
 from zope.interface import implements
 from zope.publisher.interfaces import IPublishTraverse, NotFound
@@ -19,7 +22,7 @@ class GetAllocationView(BrowserView):
         """
         Ensure user has at least n items allocated to them, say what they are.
         """
-        count = self.request.get("count", 1);
+        count = int(self.request.get("count", 1));
         if count > 40:
             return ValueError("Cannot fetch more than 40");
         quiz = Quiz(
@@ -59,16 +62,59 @@ class GetQuestionView(BrowserView):
             '/'.join(self.context.getPhysicalPath()),
             getSecurityManager().getUser().getUserName(),
         )
-        qn = quiz.getQuestionObject(uid)
-        if qn is None:
+        qnLocation = quiz.getQuestionLocation(uid)
+        if qnLocation is None:
             raise NotFound(self, uid, self.request)
+        #TODO: Is this wise?
+        qn = getSite().__parent__.unrestrictedTraverse(qnLocation)
 
         self.request.response.setHeader("Content-type", "application/json")
+        # Answers: See InvisibleQuestion.makeNewTest()
+        # grid = qn.getWrappedField('AnswerList')
+        # rowrandom = [a['answerid'] for a in grid.search(self, randomize='1')]
+        # rownotrandom = [a['answerid'] for a in grid.search(self, randomize='')]
+        # qn.isRandomOrder() randomise rowrandom.
+        # Answerids = rowrandom + rownotrandom
+        # See: Products/TutorWeb/QuizResult.py
+        # rowcorrect = grid.search(newq, correct='1')
+        # answerHtmlDict = qn.getAnswerDisplay()
+        # Populated by initializeObject() => qn.setQuestionAndAnswer() => qn.transformQuizQuestion()
+        grid = qn.getWrappedField('AnswerList')
+        if qn.isRandomOrder():
+            random_order = [a['answerid'] for a in grid.search(qn, randomize='1')]
+            fixed_order = [a['answerid'] for a in grid.search(qn, randomize='')]
+        else:
+            random_order = []
+            fixed_order = [a['answerid'] for a in grid.search(qn, randomize='')]
+            fixed_order =  [a['answerid'] for a in grid.search(qn, randomize='1')]
         return json.dumps(dict(
-            questiondata=qn.getQuestionData(),
-        ), sort_keys=True, indent=4) #TODO: Probably don't want this in real life
+            question=qn.getQuestionData(),
+            inline_answer=qn.inlineAnswer(),
+            explination=qn.getQuestionExplanationData(),
+            answer=dict(
+                html=dict(qn.getAnswerDisplay().items()),
+                random_order= random_order,
+                fixed_order= fixed_order,
+            ),
+        ))
 
 
 class UpdateScoresView(BrowserView):
     def __call__(self):
-        return "TODO"
+        answers = [dict( #TODO: Expect a JSON structure lie this
+            allocation_id=1,
+            question_id=2,
+            student_answer=-2,
+            quiz_time=datetime.now - 5,
+            answer_time=datetime.now,
+        )]
+
+        quiz = Quiz(
+            '/'.join(self.context.getPhysicalPath()),
+            getSecurityManager().getUser().getUserName(),
+        )
+        quiz.setAnswers(answers)
+
+        #TODO: Return another allocation instead?
+        self.request.response.setHeader("Content-type", "application/json")
+        return json.dumps(dict(success=True))
--- tutorweb/quiz/quiz.py
+++ tutorweb/quiz/quiz.py
@@ -1,36 +1,124 @@
-from zope.app.component.hooks import getSite
+import logging
+import random
+import time
+
+from sqlalchemy.exceptions import InvalidRequestError
+from sqlalchemy.sql import select, and_
+
+from zope.component import getUtility
+
+from collective.lead.interfaces import IDatabase
 
 from Products.TutorWeb.interfaces import IQuestionLocator
+from Products.TutorWeb.db import AllocationInformation, QuestionInformation, StudentInformation
+
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO) #TODO:
 
 
 class Quiz(object):
     """
     Object that encapsulates a quiz a student is taking
     """
-    def __init__(self, username, lecture):
+    def __init__(self, lecture, username):
         self.username = username
-        self.lecture = lecture
+        self.lectureLoc = lecture
 
-    def getAllocation(self, count):
+        # Fetch DB connection
+        self.db = getUtility(IDatabase, name='tutorweb.quizquestioninformation')
+
+        # Does the student already exist?
+        results = (self.db.session.query(StudentInformation)
+            .filter(StudentInformation.c.student_username == username)
+            .all())
+
+        if len(results) > 1:
+            raise ValueError("Too many %s students: %d" % (username, len(results)))
+        if len(results) < 1:
+            # No student_id yet, assign one
+            #TODO: Or should we be saying ENOTENROLLED?
+            raise NotImplementedError()
+        self.student_id = results[0].student_id
+
+    def getAllocation(self, desiredCount):
         """
         Allocate (count) question, return some URIs
+
+        desiredCount: How many questions should return
         """
+        def toDict(a, qn):
+            """Convert to a dict entry"""
+            return dict(
+                student_id = a.student_id,
+                allocation_id = a.allocation_id,
+                question_uid = qn.question_unique_id,
+                allocation_time = int(time.mktime(a.allocation_time.timetuple())),
+            )
+
+        # Get existing allocation from DB
+        allocation = []
+        for a, qn in (self.db.session.query(AllocationInformation, QuestionInformation)
+            .filter(AllocationInformation.c.question_id == QuestionInformation.c.question_id)
+            .filter(AllocationInformation.c.student_id == self.student_id)
+            .filter(AllocationInformation.c.answered_flag == False)
+            .limit(desiredCount)
+            .order_by(AllocationInformation.c.allocation_time)
+            .all()):
+            allocation.append(toDict(a, qn))
+
+        # If already have enough, stop
+        if len(allocation) >= desiredCount:
+            return allocation
+
+        # Get heap of questions for this lecture.
+        #TODO: .with_only_columns
+        results = (self.db.session.query(QuestionInformation)
+            .filter(QuestionInformation.c.question_location.like(self.lectureLoc + '/%'))
+            .filter(QuestionInformation.c.question_id)
+            .filter("""question_id NOT IN (
+                SELECT question_id FROM allocation_information
+                WHERE student_id = %d AND answered_flag = 0
+                )""" % self.student_id) # SQLAlchemy 0.4 can't do this.
+            .all())
+
+        while (len(allocation) < desiredCount) and (len(results) > 0):
+            #TODO: An actual IAA
+            qn = results.pop(random.randrange(len(results)))
+
+            # Create an allocation object, and add it to DB
+            a = AllocationInformation(self.student_id, self.lectureLoc, qn.question_id)
+            self.db.session.add(a)
+            self.db.session.flush()
+            allocation.append(toDict(a, qn))
+        return allocation
+
+    def getQuestionLocation(self, uid):
+        """
+        Return question associated with UID, checking that student is allowed
+        access to this question.
+        """
+        # Check that user has been allocated this question
+        try:
+            #TODO: Only return question_location
+            (a, qn) = (self.db.session.query(AllocationInformation, QuestionInformation)
+                .filter(AllocationInformation.c.question_id == QuestionInformation.c.question_id)
+                .filter(AllocationInformation.c.student_id == self.student_id)
+                .filter(AllocationInformation.c.answered_flag == False)
+                .filter(QuestionInformation.c.question_unique_id == uid)
+                .one())
+            return str(qn.question_location)
+        except InvalidRequestError, e:
+            return None
+
+    def setAllocationAnswers(self, answers):
+        """
+        Update answers
+            allocation_id=1,
+            question_id=2,
+            student_answer=-2,
+            quiz_time=datetime.now - 5,
+            answer_time=datetime.now,
+        """
+        # Unallocate all questions we've now answered (complain if DB doesn't think they were allocated)
+        # Write answer to quiz_information
+        # Update question_information
         pass
-
-    def getQuestionObject(self, uid):
-        """
-        Return question object associated with UID
-        """
-        questlocator = getUtility(IQuestionLocator)
-        questioninfo = questlocator.question_by_uid(uid)
-        if questioninfo is None:
-            raise NotFound(self, uid, self.request)
-
-        #TODO: This isn't what we want, should be
-        # * fetching via. quiz_id
-        # * WHERE answer_time IS NULL
-        # * joining with student_information.student_username.
-        # So student can only get question that 
-        qn = getSite().__parent__.unrestrictedTraverse(
-            str(questioninfo.question_location))
-        return qn

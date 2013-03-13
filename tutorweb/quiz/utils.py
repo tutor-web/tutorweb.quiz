16011114cc908e7ba2e36340d427dfccb432b077
Jamie Lentin <jm@lentin.co.uk>
2013-03-13 15:19:24 +0000

    Not a class method

--- tutorweb/quiz/utils.py
+++ tutorweb/quiz/utils.py
@@ -2,7 +2,7 @@ from zope.component import getUtility
 
 from collective.lead.interfaces import IDatabase
 
-def getDatabase(self):
+def getDatabase():
     """
     Fetch database from the registry. Ideally this should be in a 
     tutorweb.storage

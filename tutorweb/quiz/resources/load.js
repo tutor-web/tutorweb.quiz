70de02f63496bd96a750a99b6647ae01de82eb3d
Jamie Lentin <jm@lentin.co.uk>
2013-08-13 10:26:35 +0100

    Download entire tutorial in one go

--- tutorweb/quiz/resources/load.js
+++ tutorweb/quiz/resources/load.js
@@ -3,7 +3,7 @@
 
 (function (window, $) {
     "use strict";
-    var quiz, jqStatus, jqBar;
+    var quiz, qs, handleError, jqStatus, jqBar;
 
     // Wire up quiz object
     quiz = new Quiz($, localStorage, function (message) {
@@ -17,50 +17,73 @@
         jqStatus[0].className = state;
         jqStatus.text(message);
         if (state === 'ready') {
-            jqBar.css({"width": "100%"});
             $('#tw-proceed').addClass("ready");
         }
     }
 
-    function handleError(message) {
-        updateState('error', message);
+    function updateProgress(cur, max) {
+        if (max === 0) {
+            jqBar.css({"width": '0%'});
+        } else if (cur < max) {
+            jqBar.css({"width": (cur / max) * 100 + '%'});
+        } else {
+            jqBar.css({"width": '100%'});
+        }
     }
 
-    /** Download all questions associated to lecture */
-    function downloadQuestions(url) {
-        updateState("active", "Downloading lectures...");
+    handleError = function (jqXHR, textStatus, errorThrown) {
+        if (arguments.length === 3) {
+            updateState('error', errorThrown + " (whilst requesting " + this.url + ")");
+        } else {
+            // Just a string
+            updateState('error', jqXHR);
+        }
+    };
+
+    /** Download a tutorial given by URL */
+    function downloadTutorial(url) {
         $.ajax({
             type: "GET",
             cache: false,
             url: url,
             error: handleError,
             success: function (data) {
-                quiz.insertQuestions(data);
-                updateState("ready", "Press the button to start your quiz");
+                var questionDfds, count = 0;
+                quiz.insertTutorial(data.uri, data.title, data.lectures);
+
+                // Download all lecture questions in parallel
+                updateState("active", "Downloading lecture questions...");
+                questionDfds = data.lectures.map(function (l) {
+                    return $.ajax({
+                        type: "GET",
+                        cache: false,
+                        url: l.question_uri,
+                        error: handleError,
+                        success: function (data) {
+                            count += 1;
+                            updateProgress(count, questionDfds.length);
+                            quiz.insertQuestions(data);
+                        },
+                    });
+                });
+                $.when.apply(null, questionDfds).done(function () {
+                    updateProgress(1, 1);
+                    updateState("ready", "Press the button to start your quiz");
+                });
             },
         });
-    }
-
-    /** Download a lecture given by URL */
-    function downloadLecture(url) {
         updateState("active", "Downloading questions...");
-        $.ajax({
-            type: "GET",
-            cache: false,
-            url: url,
-            error: handleError,
-            success: function (data) {
-                quiz.insertTutorial('moo:TODO', 'Tutorial Title', [data]);
-                $('#tw-proceed').attr('href', quiz.quizUrl('moo:TODO', url));
-                if (data.question_uri) {
-                    downloadQuestions(data.question_uri);
-                } else {
-                    updateState("error", "No link to questions");
-                }
-            },
-        });
     }
 
-    window.localStorage.clear(); //TODO: Hack!
-    downloadLecture(window.location.search.replace(/^\??/, ""));
+    qs = quiz.parseQS(window.location);
+    if (!qs.tutUri || !qs.lecUri) {
+        handleError("Missing tutorial or lecture URI!");
+        return;
+    }
+    if (qs.clear) {
+        // Empty localStorage first
+        window.localStorage.clear();
+    }
+    $('#tw-proceed').attr('href', quiz.quizUrl(qs.tutUri, qs.lecUri));
+    downloadTutorial(qs.tutUri);
 }(window, jQuery));

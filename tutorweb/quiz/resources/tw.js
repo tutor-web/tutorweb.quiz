05a63231c3b2c3333998dbb869e6c5394037e224
Jamie Lentin <jm@lentin.co.uk>
2014-09-22 17:42:35 +0100

    Check window.navigator.onLine for any ajax calls

--- tutorweb/quiz/resources/tw.js
+++ tutorweb/quiz/resources/tw.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*jslint nomen: true, plusplus: true, browser:true */
-/*global module, require */
+/*global module, require, window */
 var Promise = require('es6-promise').Promise;
 
 /**
@@ -50,6 +50,10 @@ module.exports = function AjaxApi(jqAjax) {
     /** Call $.ajax with given arguments, return promise-wrapped output */
     this.ajax = function (args) {
         return new Promise(function (resolve, reject) {
+            if (window.navigator && !window.navigator.onLine) {
+                reject(new Error("tutorweb::error::Currently offline"));
+            }
+
             jqAjax(args).then(function (data) {
                 resolve(data);
             }).fail(function (jqXHR, textStatus, errorThrown) {

fa75c76492bf45845aaf80938669b55dd4455a6b
Jamie Lentin <jm@lentin.co.uk>
2014-09-22 15:45:25 +0100

    Move MathJax config to a separate file

---
+++ tutorweb/quiz/resources/mathjax-config.js
@@ -0,0 +1,15 @@
+MathJax.Hub.Config({
+    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+    extensions: ["tex2jax.js", "MathMenu.js", "MathZoom.js"],
+    TeX: {
+        extensions: ["AMSmath.js", "AMSsymbols.js", "noErrors.js", "noUndefined.js", "cancel.js"]
+    },
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+        displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+        ignoreClass: "jax-ignore",
+        processEscapes: true
+    }
+});
+
+MathJax.Ajax.loadComplete("[MathJax]/config/../../mathjax-config.js");

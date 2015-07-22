aff1f9ceb181ba2afaf9781d2d6bc445e18d3d6a
Jamie Lentin <jm@lentin.co.uk>
2015-07-22 16:20:07 +0100

    Expand MathJaX support with lots of hacky regexes

---
+++ tests/test_rendertex.js
@@ -0,0 +1,25 @@
+/*TODO:
+lksdflksdflk\\indent
+
+\noindent New paragraph that \\ has a line $break$ and another \\
+here and \newline here, but not
+here.
+
+Space. \vspace{}. Gotta go to \hspace{moo} space
+$$
+display = \{mode\}
+$$
+
+% You can't see me
+\begin{verbatim}
+belh
+$fish$
+
+\textbf{Whee}
+$$
+\{Apricot
+$$
+moo
+% I'm hiding at the bottoms
+\end{verbatim}
+*/

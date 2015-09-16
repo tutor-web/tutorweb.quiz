9f6a5cb25a06006772a431ddb14b9186f6a5b9db
Jamie Lentin <jm@lentin.co.uk>
2015-09-16 15:27:02 +0100

    Firefox-friendly caching options

--- tutorweb/quiz/cache.py
+++ tutorweb/quiz/cache.py
@@ -5,12 +5,18 @@ from zope.globalrequest import getRequest
 
 
 def setCacheControl(response, secs=86400):
+    def expireTime(mins):
+        return formatdate(time.time() + (60 * mins))
+
     url = getRequest().getURL()
-    if '++resource++tutorweb.quiz' in url and 'mathjax/' not in url:
-        # appcache-related stuff shouldn't be assumed to be up-to-date
+    if 'tw.appcache' in url:
+        # Always check appcache
         response.setHeader('Cache-Control', 'no-cache')
-        t = time.time() + (60 * 1)
-        response.setHeader('Expires', formatdate(t, usegmt=True))
+        response.setHeader('Expires', expireTime(0.5))
+    elif '++resource++tutorweb.quiz' in url and 'mathjax/' not in url:
+        # Don't cache javascript for very long, assume appcache will take this load
+        response.setHeader('Cache-Control', 'private')
+        response.setHeader('Expires', expireTime(1))
     else:
         # Fall back to original handler
         from zope.browserresource.file import _old_setCacheControl

b7cdc8557d5921b840272449057ad167636aa99e
Jamie Lentin <jm@lentin.co.uk>
2016-07-22 14:01:29 +0100

    Move parse_qs tests out to correct location, fix up implementation differences

---
+++ tests/test_parse_qs.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var parse_qs = require('../lib/parse_qs.js').parse_qs;
+
+module.exports.testParseQS = function (test) {
+    // Can parse both hash and search
+    function parseQS(pathname, search, hash) {
+        return parse_qs({
+            pathname: pathname,
+            hash: hash,
+            search: search
+        });
+    }
+
+    // Can use ; or & as separator
+    test.deepEqual(parseQS('/quiz.html', '?moo=yes;oink=bleh&baa=maybe', '#boing'), {
+        _doc: "quiz.html",
+        _args: ["boing"],
+        moo: "yes",
+        oink: "bleh",
+        baa: "maybe"
+    })
+
+    // Empty search still works
+    test.deepEqual(parseQS('/host:000/animal.html', '', '#camel=alice;snake=sid'), {
+        _doc: "animal.html",
+        camel: "alice",
+        snake: "sid"
+    })
+
+    // Hash wins if both defined
+    test.deepEqual(parseQS('/host:000/animal.html', '?camel=george', '#camel=alice'), {
+        _doc: "animal.html",
+        camel: "alice",
+    })
+
+    // Strings decoded
+    test.deepEqual(parseQS('/host:000/animal.html', '?camel=george', '#camel=alice%20the%20camel'), {
+        _doc: "animal.html",
+        camel: "alice the camel",
+    })
+
+    test.done();
+};
+
--- tests/test_view.js
+++ tests/test_view.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var parse_qs = require('../lib/parse_qs.js').parse_qs;
 var View = require('../lib/view.js');
 
 // Do-nothing JQuery for where we're not using it.
@@ -7,52 +8,11 @@ var nullJq = function (x) {
     return x;
 }
 
-module.exports.testParseQS = function (test) {
-    // Can parse both hash and search
-    function parseQS(href, search, hash) {
-        return (new View(nullJq)).parseQS({
-            href: href,
-            hash: hash,
-            search: search
-        });
-    }
-
-    // Can use ; or & as separator
-    test.deepEqual(parseQS('http://quiz.html', '?moo=yes;oink=bleh&baa=maybe', '#boing'), {
-        _doc: "quiz.html",
-        _opt: "boing",
-        moo: "yes",
-        oink: "bleh",
-        baa: "maybe"
-    })
-
-    // Empty search still works
-    test.deepEqual(parseQS('http://host:000/animal.html', '', '#camel=alice;snake=sid'), {
-        _doc: "animal.html",
-        camel: "alice",
-        snake: "sid"
-    })
-
-    // Hash wins if both defined
-    test.deepEqual(parseQS('http://host:000/animal.html', '?camel=george', '#camel=alice'), {
-        _doc: "animal.html",
-        camel: "alice",
-    })
-
-    // Strings decoded
-    test.deepEqual(parseQS('http://host:000/animal.html', '?camel=george', '#camel=alice%20the%20camel'), {
-        _doc: "animal.html",
-        camel: "alice the camel",
-    })
-
-    test.done();
-};
-
 module.exports.testGenerateUrl = function (test) {
     function genUrl(origHref, origHash, newOpts) {
         var view = new View(nullJq);
         // Should start the state machine to do this, but meh
-        view.curUrl = view.parseQS({href: origHref, hash: origHash, search: ""});
+        view.curUrl = parse_qs({pathname: '', href: origHref, hash: origHash, search: ""});
         return view.generateUrl(newOpts);
     }
 
